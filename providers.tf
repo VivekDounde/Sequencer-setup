f1270f9cbf2990153c4906e176be7d98fe455558
VivekDounde <156088617+VivekDounde@users.noreply.github.com>
2025-09-15 17:00:49 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# kubernetes and helm providers will be configured after EKS created,
+# using the generated kubeconfig output (see outputs.tf)
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
